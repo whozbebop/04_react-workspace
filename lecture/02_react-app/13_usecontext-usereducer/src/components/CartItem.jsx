827ed679d67decb4ea14da72d943ccc1875321a1
whozbebop <whozbebop@gmail.com>
2025-10-31 17:52:50 +0900

    update

--- lecture/02_react-app/13_usecontext-usereducer/src/components/CartItem.jsx
+++ lecture/02_react-app/13_usecontext-usereducer/src/components/CartItem.jsx
@@ -5,7 +5,14 @@ import { cartContext } from '../App'
 // <Cartitem item={장바구니아이템객체} />
 function CartItem({ item }) { // item === {id: xx, name: xxx, price, quantity: xxx}
 
-  const {dispatch} = useContext(cartContext)
+  //const {dispatch} = useContext(cartContext)
+  const dispatch = useDispatch()
+
+  // dispatch(addToCart({
+  //   id: product.id,
+  //   name: product.name,
+  //   price: product.price
+  // }))
 
   const handleItemRemove = () => {
     dispatch({type: 'REMOVE_FROM_CART', payload: item.id})
